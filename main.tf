42a58fcbe66228ff5df76ba6893840e7dbf6a454
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-09-11 05:16:54 +0530

    removed .tfvar file and formatted teraform code

--- main.tf
+++ main.tf
@@ -5,78 +5,78 @@ provider "aws" {
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 
-  enable_dns_support = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name= "terraform-server-vpc"
+    Name = "terraform-server-vpc"
   }
 }
 
 resource "aws_subnet" "this" {
-  vpc_id = aws_vpc.this.id
+  vpc_id     = aws_vpc.this.id
   cidr_block = var.subnet_cidr
 
-  availability_zone = var.availability_zone
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
-    Name= "terraform-server-subnet"
+    Name = "terraform-server-subnet"
   }
 }
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-  
+
   tags = {
-    Name= "terraform-server-igw"
+    Name = "terraform-server-igw"
   }
 }
 
 resource "aws_route_table" "this" {
-  vpc_id =  aws_vpc.this.id
+  vpc_id = aws_vpc.this.id
 
   tags = {
-    Name= "terraform-server-route-table"
+    Name = "terraform-server-route-table"
   }
 }
 
 resource "aws_route" "this" {
-  route_table_id = aws_route_table.this.id
+  route_table_id         = aws_route_table.this.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.this.id
+  gateway_id             = aws_internet_gateway.this.id
 }
 
 resource "aws_route_table_association" "this" {
-    subnet_id = aws_subnet.this.id
+  subnet_id      = aws_subnet.this.id
   route_table_id = aws_route_table.this.id
 }
 
 resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
-  name= "terraform-server-sg"
-  
+  name   = "terraform-server-sg"
+
   ingress {
     description = "HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All-trafic"
-    from_port = 0
-    to_port = 0
-    protocol = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -87,15 +87,15 @@ resource "aws_security_group" "this" {
 
 resource "aws_instance" "this" {
 
-  ami = "ami-0b6d9d3d33ba97d99"
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.this.id
+  ami                         = "ami-0b6d9d3d33ba97d99"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.this.id
   associate_public_ip_address = true
-  user_data = file("user_data.sh")
+  user_data                   = file("user_data.sh")
 
   vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
-    Name= "terraform-server"
+    Name = "terraform-server"
   }
 }
